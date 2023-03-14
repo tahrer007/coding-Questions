123356294e6343ac92eb9975e71bd9dbf5f206ed
tahrer007 <tahrer_Ad@outlook.com>
2023-03-14 22:54:39 +0200

    climbStairs

---
+++ leetcode/algorithms/climbStairs.js
@@ -0,0 +1,11 @@
+//70. Climbing Stairs
+var climbStairs = function(n, cache = {}) {
+    if(n <= 0) return 0;
+    if(n === 1) return 1;
+    if(n === 2) return 2;
+    if(cache[n]) return cache[n]; // Check if result is already cached
+    
+    const result = climbStairs(n-1, cache) + climbStairs(n-2, cache);
+    cache[n] = result; // Cache the result for future use
+    return result;
+};
